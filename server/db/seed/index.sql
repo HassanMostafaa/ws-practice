29a986c54a1948253d9c6596532345f850fd6b0b
HassanMostafaa <hassan.mohamed@evolvextechnology.com>
2026-04-15 13:25:15 +0400

    remote db integration with more seed

--- server/db/seed/index.sql
+++ server/db/seed/index.sql
@@ -14,3 +14,14 @@ INSERT INTO notes (title, content, username) VALUES ('First note', 'This is my f
 INSERT INTO notes (title, content, username) VALUES ('Second note', 'This is my second note content passed from server db seed', 'hassan');
 INSERT INTO notes (title, content, username) VALUES ('Third note', 'This is my third note content passed from server db seed', 'hassan');
 INSERT INTO notes (title, content, username) VALUES ('Fourth note', 'This is my fourth note content passed from server db seed', 'hassan');
+INSERT INTO notes (title, content, username) VALUES ('Fifth note', 'This is my fifth note content passed from server db seed', 'hassan');
+INSERT INTO notes (title, content, username) VALUES ('Sixth note', 'This is my sixth note content passed from server db seed', 'hassan');
+INSERT INTO notes (title, content, username) VALUES ('Seventh note', 'This is my seventh note content passed from server db seed', 'hassan');
+INSERT INTO notes (title, content, username) VALUES ('Eighth note', 'This is my eighth note content passed from server db seed', 'hassan');
+INSERT INTO notes (title, content, username) VALUES ('Ninth note', 'This is my ninth note content passed from server db seed', 'hassan');
+INSERT INTO notes (title, content, username) VALUES ('Tenth note', 'This is my tenth note content passed from server db seed', 'hassan');
+INSERT INTO notes (title, content, username) VALUES ('Eleventh note', 'This is my eleventh note content passed from server db seed', 'hassan');
+INSERT INTO notes (title, content, username) VALUES ('Twelfth note', 'This is my twelfth note content passed from server db seed', 'hassan');
+INSERT INTO notes (title, content, username) VALUES ('Thirteenth note', 'This is my thirteenth note content passed from server db seed', 'hassan');
+INSERT INTO notes (title, content, username) VALUES ('Fourteenth note', 'This is my fourteenth note content passed from server db seed', 'hassan');
+INSERT INTO notes (title, content, username) VALUES ('Fifteenth note', 'This is my fifteenth note content passed from server db seed', 'hassan');
